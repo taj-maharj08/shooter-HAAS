19f251a56b9883ad490a5377f03682ccf7106b35
taj-maharj08 <arjunmahajan@billericak12.com>
2023-12-07 19:24:41 -0500

    untested feedforward

--- Shooter HAAS/src/main/java/frc/robot/Shooter.java
+++ Shooter HAAS/src/main/java/frc/robot/Shooter.java
@@ -1,15 +1,20 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
     private CANSparkMax m_leftRoller, m_rightRoller, m_flywheelMotorPrimary, m_flywheelMotorSecondary, m_flywheelTertiary;
-
+    private SimpleMotorFeedforward feedforward;
+    private final double goalVel = 5000;
     public Shooter() {
+        feedforward = new SimpleMotorFeedforward(0.1, 0.008, 0);
+
         m_leftRoller = new CANSparkMax(0, MotorType.kBrushless);
         m_rightRoller = new CANSparkMax(1, MotorType.kBrushless);
         m_flywheelMotorPrimary = new CANSparkMax(2, MotorType.kBrushless);
@@ -23,14 +28,19 @@ public class Shooter extends SubsystemBase {
     }
 
     public Command shoot(){
-        return this.runOnce(() -> {
-            m_flywheelMotorPrimary.set(0.1);
+        return this.run(() -> {
+            double ft = feedforward.calculate(goalVel);
+            m_flywheelMotorPrimary.getPIDController().setReference(goalVel, ControlType.kVelocity, 0, ft);
             m_rightRoller.set(0.5);
         }).withName("shoot");
     }
 
+    public double getSpeed(){
+        return m_flywheelMotorPrimary.getEncoder().getVelocity();
+    }
+
     public Command idle() {
-        return this.runOnce(() -> {
+        return this.run(() -> {
             m_flywheelMotorPrimary.set(0);
             m_rightRoller.set(0);
         }).withName("Idle");
